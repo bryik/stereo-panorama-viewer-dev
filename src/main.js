b173079be715ba92d5237d0d6566b7a5980af8ef
Ben Pyrik <ben@wsundine.com>
2020-07-17 19:34:33 -0500

    Import A-Frame, components by <script>
    Importing via NPM caused noticably slow bundling.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,5 @@
 import App from "./App.svelte";
 
-// Initialize A-Frame
-import "aframe";
-import "./aframe-components/aframe-overunder-component";
-import "./aframe-components/aframe-stereocam-component";
-
 const app = new App({
   target: document.body,
 });
